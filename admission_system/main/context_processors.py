077d760a2c3b0f4569678bdbab3caf6d0180780c
dibashgautam <dibashgautam143@gmail.com>
2025-11-30 23:06:15 +0545

    Delete admission_system directory

--- admission_system/main/context_processors.py
+++
@@ -1,9 +0,0 @@
-from main.models import Institute
-
-def approved_institute(request):
-    user_student = getattr(request.user, 'student_profile', None)
-    approved = None
-    if user_student:
-        institutes = Institute.objects.filter(owner=user_student, status='approved')
-        approved = institutes.first() if institutes.exists() else None
-    return {'approved_institute': approved}
